18ae0ac963a696d85a654e46b56bbffaed2ef8cb
NenyeIkpa <82512623+NenyeIkpa@users.noreply.github.com>
2024-05-03 02:10:38 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,88 @@
 #!/usr/bin/python3
-"""
-module contains a script that reads stdin line by line and computes metrics
-"""
-import sys
+'''A script for parsing HTTP request logs.
+'''
+import re
 
 
-def print_values(file_total_size, status_codes):
-    print('File size: {}'.format(file_total_size))
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-    for status_code in sorted(status_codes):
-        if status_codes[status_code] > 0:
-            print('{}: {}'.format(status_code, status_codes[status_code]))
+
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def main():
-    file_total_size = 0
-    status_codes = {}
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
 
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        line_count = 0
-
-        # Read input from stdin
-        for line in sys.stdin:
-            line_count += 1
-
-            parts = line.strip().split()
-            if (len(parts) != 9):
-                return
-            [ip_address, dash, date1,
-             date2, request1, request2, request3,
-             status_code, file_size] = parts[:9]
-
-            if not status_code.isdigit():
-                return
-            status_code = int(status_code)
-            file_size = int(file_size)
-            file_total_size += file_size
-
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            else:
-                status_codes[status_code] = 1
-
-            if line_count % 10 == 0:
-                print_values(file_total_size, status_codes)
-
-    except KeyboardInterrupt:
-        print_values(file_total_size, status_codes)
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    run()
