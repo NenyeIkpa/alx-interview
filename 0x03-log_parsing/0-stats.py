3e177f9f9555919239a46a0e272ddfbb04aebbcd
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-03 01:35:31 +0100

    task 0 update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 module contains a script that reads stdin line by line and computes metrics
@@ -6,8 +5,44 @@ module contains a script that reads stdin line by line and computes metrics
 import sys
 
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
+def print_values(file_total_size, status_codes):
+    print('File size: {}'.format(file_total_size))
+
+    for status_code in sorted(status_codes):
+        if status_codes[status_code] > 0:
+            print('{}: {}'.format(status_code, status_codes[status_code]))
+
+
+file_total_size = 0
+status_codes = {}
+
+try:
+    line_count = 0
+
+    # Read input from stdin
+    for line in sys.stdin:
+        line_count += 1
+
+        parts = line.strip().split()
+        if (len(parts) == 9):
+            [ip_address, dash, date1,
+             date2, request1, request2, request3,
+             status_code, file_size] = parts[:9]
+
+            if status_code.isdigit():
+                status_code = int(status_code)
+                file_size = int(file_size)
+
+                file_total_size += file_size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                else:
+                    status_codes[status_code] = 1
+
+                if line_count % 10 == 0:
+                    print_values(file_total_size, status_codes)
+        else:
+            print("len(parts) is more than 6. display {}".format(parts))
+
+except KeyboardInterrupt:
+    print_values(file_total_size, status_codes)
